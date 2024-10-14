6b4455917d133cff70eca0e210dabf31ddd79c4f
Denys Reshetnyk <denus.reshetnuk@gmail.com>
2024-10-14 09:23:50 +0300

    fix: suspense

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+    },
     // output: "export",
     webpack(config) {
         // Grab the existing rule that handles SVG imports
